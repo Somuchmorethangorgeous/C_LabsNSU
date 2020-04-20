72d229f6c3475dd883d2cfe2c755f75394a7bd3d
Somuchmorethangorgeous <ruslanchik.popov@gmail.com>
2020-04-21 00:47:38 +0700

    upload

---
+++ Translate2-16/src/Translation.h
@@ -0,0 +1,22 @@
+//
+// Created by roxxa on 30.11.2019.
+//
+
+#ifndef INC_1_TRANSLATE2_16_TRANSLATION_H
+#define INC_1_TRANSLATE2_16_TRANSLATION_H
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+int verify_system(int s1, int s2);
+int cifry(char*a, int n, int sys1);
+int verify_len(char* a);
+int dot (char* a);
+int verify_symb (char*s);
+void final_ch2(double z_part, int b2);
+void final_ch1(long long int ch, int razmer, int b2);
+void z_part(char*a, int b1, int b2, int c);
+void d_part(char*a, int b1, int b2, int c, int d);
+
+#endif //INC_1_TRANSLATE2_16_TRANSLATION_H
